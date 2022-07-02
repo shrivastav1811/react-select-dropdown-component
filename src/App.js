33def2f9260c564d958b7345fadc786e15f81fc1
shrivastav1811 <sonushrivastav1811@gmail.com>
2022-07-02 21:17:55 +0530

    gggffgdjfg

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useMemo } from 'react'
+import Select from 'react-select'
+import countryList from 'react-select-country-list'
+import SelectCurrency from 'react-select-currency'
+import PhoneInput from 'react-phone-input-2'
+import 'react-phone-input-2/lib/style.css'
 
 function App() {
+
+  // country select
+  const [value, setValue] = useState('')
+  const options = useMemo(() => countryList().getData(), [])
+  const changeHandler = value => {
+    setValue(value)
+  }
+
+  // currency select
+  const onSelectedCurrency = currencyAbbrev => {
+    console.log((`Selected ${currencyAbbrev}`));
+}
+
+  // react select
+  const [selectedOption, setSelectedOption] = useState();
+  const option = [
+    { value: 'chocolate', label: 'Chocolate' },
+    { value: 'strawberry', label: 'Strawberry' },
+    { value: 'vanilla', label: 'Vanilla' },
+  ];
+  
+  console.log(value);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className>
+      <Select options={options} value={value} onChange={changeHandler} className="select" />
+      {/* <SelectCurrency value="" onCurrencySelected={onSelectedCurrency} /> */}
+      <PhoneInput
+        inputProps={{
+    country:'us',
+    name: 'phone',
+    required: true,
+    autoFocus: true
+        }}
+        className="select"          
+      />
+      <Select
+        defaultValue={selectedOption}
+        onChange={setSelectedOption}
+        options={option}
+        className="select"
+      />
     </div>
   );
 }
